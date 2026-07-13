1950abd41231f1972da01ed57c786d020a7c414a
Naveen Kumar <naveenkumar139117@gmail.com>
2026-07-13 17:54:17 +0530

    Startup

---
+++ script.js
@@ -0,0 +1,69 @@
+ 
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+ 
+const hamburger = document.getElementById('hamburger');
+const navWrapper = document.getElementById('nav-wrapper');
+const navLinks = document.querySelectorAll('.nav-links a');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navWrapper.classList.toggle('active');
+
+ 
+    if (navWrapper.classList.contains('active')) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = '';
+    }
+});
+
+ 
+navLinks.forEach(link => {
+    link.addEventListener('click', function () {
+ 
+        navLinks.forEach(l => l.classList.remove('active'));
+
+ 
+        this.classList.add('active');
+
+ 
+        if (navWrapper.classList.contains('active')) {
+            hamburger.classList.remove('active');
+            navWrapper.classList.remove('active');
+            document.body.style.overflow = '';
+        }
+    });
+});
+
+
+
+ 
+document.addEventListener("DOMContentLoaded", () => {
+    const observerOptions = {
+        root: null,
+        rootMargin: '0px',
+        threshold: 0.1 // Triggers when 10% of the element is visible
+    };
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+                observer.unobserve(entry.target); // Stop observing once it's visible
+            }
+        });
+    }, observerOptions);
+
+ 
+    const footerElements = document.querySelectorAll('.footer-fade-in');
+    footerElements.forEach(el => observer.observe(el));
+});
